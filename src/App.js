9271a51dca7f5d2a8145175d1804ef50bd6b6d23
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-11-21 18:09:50 +0600

    Start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Button, Container } from '@mui/material';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "React Material UI - Home";
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container
+      disableGutters
+      maxWidth = "xl"
+      SX = {{
+        background: "#fff",
+      }}
+      >
+        <Button variant='contained'>Test</Button>
+      </Container>
     </div>
   );
 }
